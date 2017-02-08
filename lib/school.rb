4fa3ec5fc89aaf9fb1c4bd3966751b8024134b99
rmody3 <rmody3@gmail.com>
2017-02-08 12:02:01 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+
+class School
+
+  attr_reader :school_name, :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = Hash.new([])
+  end
+
+
+  def add_student(student_name, grade)
+    @roster[grade] += [student_name]
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = @roster.each do |k,v|
+      v.sort!
+    end
+    sorted
+  end
+end
